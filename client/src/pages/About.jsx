11ce2c4da39e4ba8a45eab4dc39e41c2a02a16f0
Davis Song <davissong01@gmail.com>
2023-06-01 17:06:21 -0700

    feat: implement global nav bar with routing

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+export default function About() {
+  return (
+    <Typography variant="h1">
+      About Page
+    </Typography>
+  );
+}
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+export default function Home() {
+  return (
+    <Typography variant="h1">
+      Home Page
+    </Typography>
+  );
+}
