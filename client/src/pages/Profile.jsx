a6901a3fb9a62c4ce1038e8028e0ae33caffe02f
kennyjhcheng <kennyjiahuacheng@gmail.com>
2023-06-02 20:24:29 -0700

    feat: profile upload component

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Typography } from '@mui/material';
+import AvatarUpload from '../components/Profile/AvatarUpload';
 
 export default function Profile() {
   return (
-    <Typography variant="h1">
-      Profile Page
-    </Typography>
+    <>
+      <Typography variant="h1">
+        Profile Page
+      </Typography>
+      <AvatarUpload />
+    </>
   );
 }
