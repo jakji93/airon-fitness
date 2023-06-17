e6bbcbb97fb544740a290fa13059e85ec4f222db
kennyjhcheng <kennyjiahuacheng@gmail.com>
2023-06-17 13:51:17 -0700

    fix: changed weekly_availability_options to 1-7

--- client/src/constants/AdditionalProfile.js
+++ client/src/constants/AdditionalProfile.js
@@ -79,13 +79,13 @@ export const ALLERGIES_INTOLERANCES_OPTIONS = {
 export const allergiesIntolerancesOptions = Object.values(ALLERGIES_INTOLERANCES_OPTIONS);
 
 export const WEEKLY_AVAILABILITY_OPTIONS = {
-  SUNDAY: '0',
-  MONDAY: '1',
-  TUESDAY: '2',
-  WEDNESDAY: '3',
-  THURSDAY: '4',
-  FRIDAY: '5',
-  SATURDAY: '6',
+  SUNDAY: '1',
+  MONDAY: '2',
+  TUESDAY: '3',
+  WEDNESDAY: '4',
+  THURSDAY: '5',
+  FRIDAY: '6',
+  SATURDAY: '7',
 };
 
 export const weeklyAvailabilityOptions = Object.values(WEEKLY_AVAILABILITY_OPTIONS);
