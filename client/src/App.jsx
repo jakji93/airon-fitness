a4b4577e6e88f972018d07128c77de478cb320c4
kennyjhcheng <kennyjiahuacheng@gmail.com>
2023-06-24 01:29:15 -0700

    feat: redirect to login from /app/ routes if not logged in

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
 import {
   CssBaseline,
 } from '@mui/material';
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 import ResponsiveAppBar from './components/Navbar';
 
 export default function App() {
+  const {
+    user,
+  } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) navigate('/login');
+  }, [user]);
+
   return (
     <>
       <CssBaseline />
