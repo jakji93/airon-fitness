35cb06d2f6eb5c524267ec7dfaf85593d3c06618
Davis Song <davissong01@gmail.com>
2023-06-29 21:31:31 -0700

    feat: migrate Kevin's openAIUtil and use it in mealSchedule

--- server/src/controllers/mealScheduleController.js
+++ server/src/controllers/mealScheduleController.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const asyncHandler = require('express-async-handler');
+const openAI = require('../utils/openaiUtil');
 const MealSchedule = require('../models/MealScheduleModel');
 const { mockMealSchedule } = require('../mock/MealScheduleMockData');
 
@@ -42,49 +43,67 @@ const createMealScheduleForUser = asyncHandler(async (req, res) => {
     throw new Error('Meal schedule already exists');
   }
 
+  // Generate the schedule
+  const tempUser = {
+    age: 25,
+    sex: 'male',
+    weight: '186',
+    BMI: '23',
+    fitness: 'high',
+    healthConditions: 'asthma',
+    height: '180',
+    timePreference: '5 days per week',
+    durationPreference: '60',
+    equipmentAcess: 'dumbbells',
+    goal: 'weight loss'
+  }
+  console.log("before the axios call");
+  const generatedSchedule = await openAI.generateMealSchedule(tempUser);
+
   // Create meal in MongoDB
   const mealSchedule = await MealSchedule.create({
     _userInfoID: id,
+    schedule: generatedSchedule,
+    inputs: [],
     // schedule: mockMealSchedule,
     // schedule: {},
-    schedule: {
-      Monday: {
-        breakfast: 'Oatmeal with berries and a sprinkle of nuts',
-        snack1: 'Greek yogurt with cucumber slices',
-        lunch: 'Grilled chicken breast with mixed vegetables',
-        snack2: 'Apple slices with almond butter',
-        dinner: 'Salmon with quinoa and steamed broccoli',
-      },
-      Tuesday: {
-        breakfast: 'Egg white omelette with spinach and tomatoes',
-        snack1: 'Protein shake with almond milk',
-        lunch: 'Turkey wrap with whole wheat tortilla, lettuce, and tomato',
-        snack2: 'Carrot sticks with hummus',
-        dinner: 'Grilled lean steak with sweet potato and roasted asparagus',
-      },
-      Wednesday: {
-        breakfast: 'Whole grain toast with avocado and poached eggs',
-        snack1: 'Mixed berries',
-        lunch: 'Quinoa salad with grilled chicken, bell peppers, and feta cheese',
-        snack2: 'Celery sticks with peanut butter',
-        dinner: 'Baked cod with brown rice and sautéed zucchini',
-      },
-      Thursday: {
-        breakfast: 'Greek yogurt with granola and sliced bananas',
-        snack1: 'Protein bar',
-        lunch: 'Shrimp stir-fry with mixed vegetables and brown rice',
-        snack2: 'Edamame',
-        dinner: 'Grilled tofu with quinoa and roasted Brussels sprouts',
-      },
-      Friday: {
-        breakfast: 'Smoothie with spinach, banana, almond milk, and protein powder',
-        snack1: 'Cucumber slices with tzatziki sauce',
-        lunch: 'Grilled chicken breast with quinoa and steamed broccoli',
-        snack2: 'Mixed nuts',
-        dinner: 'Grilled salmon with sweet potato fries and grilled asparagus',
-      },
-    },
-    inputs: [],
+    // schedule: {
+    //   Monday: {
+    //     breakfast: 'Oatmeal with berries and a sprinkle of nuts',
+    //     snack1: 'Greek yogurt with cucumber slices',
+    //     lunch: 'Grilled chicken breast with mixed vegetables',
+    //     snack2: 'Apple slices with almond butter',
+    //     dinner: 'Salmon with quinoa and steamed broccoli',
+    //   },
+    //   Tuesday: {
+    //     breakfast: 'Egg white omelette with spinach and tomatoes',
+    //     snack1: 'Protein shake with almond milk',
+    //     lunch: 'Turkey wrap with whole wheat tortilla, lettuce, and tomato',
+    //     snack2: 'Carrot sticks with hummus',
+    //     dinner: 'Grilled lean steak with sweet potato and roasted asparagus',
+    //   },
+    //   Wednesday: {
+    //     breakfast: 'Whole grain toast with avocado and poached eggs',
+    //     snack1: 'Mixed berries',
+    //     lunch: 'Quinoa salad with grilled chicken, bell peppers, and feta cheese',
+    //     snack2: 'Celery sticks with peanut butter',
+    //     dinner: 'Baked cod with brown rice and sautéed zucchini',
+    //   },
+    //   Thursday: {
+    //     breakfast: 'Greek yogurt with granola and sliced bananas',
+    //     snack1: 'Protein bar',
+    //     lunch: 'Shrimp stir-fry with mixed vegetables and brown rice',
+    //     snack2: 'Edamame',
+    //     dinner: 'Grilled tofu with quinoa and roasted Brussels sprouts',
+    //   },
+    //   Friday: {
+    //     breakfast: 'Smoothie with spinach, banana, almond milk, and protein powder',
+    //     snack1: 'Cucumber slices with tzatziki sauce',
+    //     lunch: 'Grilled chicken breast with quinoa and steamed broccoli',
+    //     snack2: 'Mixed nuts',
+    //     dinner: 'Grilled salmon with sweet potato fries and grilled asparagus',
+    //   },
+    // },
   });
 
   if (mealSchedule) {
