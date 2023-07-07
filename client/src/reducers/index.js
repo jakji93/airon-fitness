3647c2790a743326a52f66227207d965b9898c55
kennyjhcheng <kennyjiahuacheng@gmail.com>
2023-07-06 17:20:34 -0700

    removed unnecessary additionalprofile and basicprofile reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,16 +1,12 @@
 import { combineReducers } from 'redux';
 
-import additionalProfileReducer from './AdditionalProfile';
 import authReducer from './Auth';
-import basicProfileReducer from './BasicProfile';
 import fitnessPlanReducer from './FitnessPlan';
 import mealPlanReducer from './MealPlan';
 import signupReducer from './Signup';
 import userProfileReducer from './UserProfile';
 
 const rootReducer = combineReducers({
-  basicProfile: basicProfileReducer,
-  additionalProfile: additionalProfileReducer,
   fitnessPlan: fitnessPlanReducer,
   auth: authReducer,
   mealPlan: mealPlanReducer,
