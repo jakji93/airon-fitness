94bdc3649fed707e6fc6c9ec9999cb51d2271a13
Davis Song <davissong01@gmail.com>
2023-07-13 17:15:21 -0700

    feat: implement macros in meal schedule and refactoring

--- server/src/utils/prompts/mealPrompts.js
+++ server/src/utils/prompts/mealPrompts.js
@@ -1,52 +1,72 @@
-const mealPrompt = (mode, user, inputs, schedule) => {
-  return [
-    {
-      role: "system",
-      content: "You are a fitness expert and dietary specialist."
-    },
-    {
-      role: "user",
-      content: mode === 'create' ? mealCreationPrompt(user) : mealUpdatePrompt(user, inputs, schedule)
-    }
-  ]
-}
+const mealCreationPrompt = (user) => `
+  Imagine a ${user.age} year old ${user.gender}, 
+  weight ${user.weight}${user.weightUnit} (assume kilograms if no unit provided), 
+  height ${user.height}${user.heightUnit} (assume centimeters if no unit provided),
+  has fitness experience of: ${user.fitness}, 
+  has body fat percentage of: ${user.bodyFat}, and
+  has muscle mass percentage of: ${user.muscleMass}. 
 
-const mealCreationPrompt = (user) => {
-  return `Imagine a ${user.age} year old ${user.gender}, 
-          weight ${user.weight}${user.weightUnit} (assume kilograms if no unit provided), 
-          height ${user.height}${user.heightUnit} (assume centimeters if no unit provided),
-          has fitness experience of: ${user.fitness}, 
-          has body fat percentage of: ${user.bodyFat}, and
-          has muscle mass percentage of: ${user.muscleMass}. 
+  Their fitness and health goals are: ${user.goals}.
+  They have the following dietary restrictions: ${user.dietRestrictions},
+  They have the following health conditions: ${user.healthConditions},
+  They have the following allergies: ${user.allergies},
 
-          Their fitness and health goals are: ${user.goals}.
-          They have the following dietary restrictions: ${user.dietRestrictions},
-          They have the following health conditions: ${user.healthConditions},
-          They have the following allergies: ${user.allergies},
+  Now, create a weekly meal plan including breakfast, snack1 (strictly use snack1 in respond), 
+  lunch, snack2 (strictly use snack2 in respond) and dinner in JSON form.
+  In the JSON, include the total amount of nutritional macros (as nutrition_totals) for each day provided by the meal plan. 
+  Don't include the nutritional macros per food item. 
+  Don't include any Note. For null values use 0 instead.
 
-          Now, create a weekly meal plan including breakfast, snack1 (strictly use snack1 in respond), 
-          lunch, snack2 (strictly use snack2 in respond) and dinner in JSON form. Don't include any Note. For null values use 0 instead.`  
-  }
+  To be explicit, the JSON should be formatted like so:
+  {
+    schedule:
+      {Monday:
+        {breakfast: string,
+         snack1: string,
+         lunch: string,
+         snack2: string
+         dinner: string
+         nutrition_totals: {
+          calories: number,
+          carbohydrates: number,
+          protein: number,
+          fat: number
+         }
+        }
+        Tuesday:...
+        Wednesday:...
+        ...}
+  }`;
 
-const mealUpdatePrompt = (user, inputs, schedule) => {
-    return `You are a dietician that is given the task to update a client's schedule.
+const mealUpdatePrompt = (user, inputs, schedule) => `
+  Imagine that you are updating a client's diet/meal schedule.
 
-            Here is some information about the client:
-            Allergies (make sure the schedule is free of these allergen risks): ${user.allergies}
-            Goals: ${user.goals}
-            Health Conditions: ${user.healthConditions}
-            Diet Restrictions: ${user.dietRestrictions}
-      
-            Here are the additional requirements or changes that the user has requested:
-            ${inputs}
+  Here is some information about the client:
+  Allergies (make sure the schedule is free of these allergen risks): ${user.allergies}
+  Goals: ${user.goals}
+  Health Conditions: ${user.healthConditions}
+  Diet Restrictions: ${user.dietRestrictions}
 
-            Please take the following existing JSON workout schedule and make the necessary tweaks based on the additional requirements or changes:
-            ${schedule}
+  Here are the additional requirements or changes that the user has requested:
+  ${inputs}
 
-            Please make the necessary modifications and return the updated workout schedule in the same JSON format. Thank you!
-            Don't include Note. For null values use 0 instead.`  
-  }
+  Please take the following existing JSON workout schedule and make the necessary tweaks based on the additional requirements or changes:
+  ${schedule}
 
-  module.exports = {
-    mealPrompt,
-  }
+  Please make the necessary modifications and return the updated workout schedule in the same JSON format. Thank you!
+  Don't include Note. For null values use 0 instead.`;
+
+const mealPrompt = (mode, user, inputs, schedule) => [
+  {
+    role: 'system',
+    content: 'You are a fitness expert and dietary specialist.',
+  },
+  {
+    role: 'user',
+    content: mode === 'create' ? mealCreationPrompt(user) : mealUpdatePrompt(user, inputs, schedule),
+  },
+];
+
+module.exports = {
+  mealPrompt,
+};
