8ec24004d14b79d1260534ccde4be292566539af
kennyjhcheng <kennyjiahuacheng@gmail.com>
2023-07-20 18:10:21 -0700

    fixed key error for collapse

--- client/src/components/Home/PlanView/ExistingMealSchedule.jsx
+++ client/src/components/Home/PlanView/ExistingMealSchedule.jsx
@@ -4,8 +4,54 @@ import {
 import {
   Collapse, Grid, List, ListItem, ListItemButton, ListItemText,
 } from '@mui/material';
+import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+
+function MealScheduleCollapse(props) {
+  const {
+    index,
+    handleClick,
+    selectedIndex,
+    dayPlan,
+  } = props;
+
+  return (
+    <div>
+      <ListItemButton key={index} onClick={() => { handleClick(index); }} divider>
+        <ListItemText primary={`Day ${index + 1}`} />
+        {index === selectedIndex ? <ExpandLess /> : <ExpandMore />}
+      </ListItemButton>
+      {dayPlan && Object.keys(dayPlan).map((meal) => (
+        <Collapse
+          in={index === selectedIndex}
+          timeout="auto"
+          unmountOnExit
+          key={`${index} ${meal.toString()}`}
+        >
+          <ListItem>
+            <ListItemText
+              secondary={`${meal.toString()}: ${dayPlan[meal]}`}
+            />
+          </ListItem>
+        </Collapse>
+      ))}
+    </div>
+  );
+}
+
+MealScheduleCollapse.propTypes = {
+  index: PropTypes.number.isRequired,
+  handleClick: PropTypes.func.isRequired,
+  selectedIndex: PropTypes.string.isRequired,
+  dayPlan: PropTypes.shape({
+    breakfast: PropTypes.string,
+    snack1: PropTypes.string,
+    lunch: PropTypes.string,
+    snack2: PropTypes.string,
+    dinner: PropTypes.string,
+  }).isRequired,
+};
 
 export default function ExistingMealSchedule() {
   const [schedule, setSchedule] = useState();
@@ -13,7 +59,6 @@ export default function ExistingMealSchedule() {
   const { mealSchedule } = useSelector(
     (state) => state.workoutAndMealSchedule,
   );
-  let counter = 0;
 
   const handleClick = (index) => {
     if (index === selectedIndex) {
@@ -36,27 +81,15 @@ export default function ExistingMealSchedule() {
           width: '100%', maxheight: '100%', overflow: 'auto', bgcolor: 'background.paper',
         }}
       >
-        {schedule && Object.keys(schedule).map((day, index) => {
-          const dayPlan = schedule[day];
-          counter += 1;
-          return (
-            <div>
-              <ListItemButton key={counter} onClick={() => { handleClick(index); }} divider>
-                <ListItemText primary={`Day ${counter}`} />
-                {index === selectedIndex ? <ExpandLess /> : <ExpandMore />}
-              </ListItemButton>
-              {dayPlan && Object.keys(dayPlan).map((meal) => (
-                <Collapse in={index === selectedIndex} timeout="auto" unmountOnExit>
-                  <ListItem>
-                    <ListItemText
-                      secondary={`${meal.toString()}: ${dayPlan[meal]}`}
-                    />
-                  </ListItem>
-                </Collapse>
-              ))}
-            </div>
-          );
-        })}
+        {schedule && Object.keys(schedule).map((day, index) => (
+          <MealScheduleCollapse
+            dayPlan={schedule[day]}
+            handleClick={handleClick}
+            index={index}
+            key={`${day} meal`}
+            selectedIndex={selectedIndex}
+          />
+        ))}
       </List>
     </Grid>
   );
