c8d038c315b23c334ea3e82f7d7ffa42c345b049
chungs07 <chungs07@students.cs.ubc.ca>
2023-07-29 17:10:27 -0700

    Remove header in home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Grid, Typography } from '@mui/material';
+import { Grid } from '@mui/material';
 import React from 'react';
 
 import ChatArea from '../components/Home/ChatArea';
@@ -7,24 +7,16 @@ import StatsView from '../components/Home/StatsView/StatsView';
 
 export default function Home() {
   return (
-    <>
-      <Typography variant="h2">
-        Welcome to Ai-ron Fitness!
-      </Typography>
-      <Typography variant="subtitle1">
-        Where AI Whips You into Shape and Leaves You Bot-ter than Ever!
-      </Typography>
-      <Grid container sx={{ p: 2 }} spacing={4}>
-        <Grid item xs>
-          <TabbedScheduleView />
-        </Grid>
-        <Grid item xs>
-          <StatsView />
-        </Grid>
-        <Grid item xs={4.5}>
-          <ChatArea />
-        </Grid>
+    <Grid container sx={{ p: 2 }} spacing={4}>
+      <Grid item xs>
+        <TabbedScheduleView />
       </Grid>
-    </>
+      <Grid item xs>
+        <StatsView />
+      </Grid>
+      <Grid item xs>
+        <ChatArea />
+      </Grid>
+    </Grid>
   );
 }
