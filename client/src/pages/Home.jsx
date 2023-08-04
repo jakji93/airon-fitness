90ee3216f8a2e0f2a5a8a7040be102364ff155e7
kennyjhcheng <kennyjiahuacheng@gmail.com>
2023-08-04 01:01:23 -0700

    added browser notification when requests are fulfilled, and request for notification permissions

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { Grid } from '@mui/material';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import ChatArea from '../components/Home/ChatArea/ChatArea';
 import TabbedScheduleView from '../components/Home/PlanView/TabbedScheduleView';
@@ -7,6 +7,10 @@ import StatsView from '../components/Home/StatsView/StatsView';
 import WorkoutTable from '../components/Home/WorkoutTable';
 
 export default function Home() {
+  useEffect(() => {
+    if ('Notification' in window) Notification.requestPermission();
+  }, []);
+
   return (
     <Grid container sx={{ p: 3 }} spacing={5}>
       <Grid item xs={12} sm={6}>
