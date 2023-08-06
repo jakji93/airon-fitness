cd2a92e0c3591ec197c0ec6977f21fb10402edad
chungs07 <chungs07@students.cs.ubc.ca>
2023-08-05 23:47:07 -0700

    Style ExistingWorkoutTable message

--- client/src/components/Home/WorkoutTable/ExistingWorkoutTable.jsx
+++ client/src/components/Home/WorkoutTable/ExistingWorkoutTable.jsx
@@ -1,16 +1,14 @@
 import {
-  Card, Grid, Typography,
+  Grid, Typography,
 } from '@mui/material';
 import React from 'react';
 
 export default function ExistingWorkoutTable() {
   return (
-    <Grid container component={Card} alignItems="center" sx={{ p: 2 }}>
-      <Grid item xs={12}>
-        <Typography variant="h5" component="span" color="text.secondary">
-          Click here to start your workout!
-        </Typography>
-      </Grid>
+    <Grid container alignItems="center" justifyContent="center" sx={{ p: 2 }}>
+      <Typography variant="h5" component="span" color="text.secondary">
+        Click here to start your workout!
+      </Typography>
     </Grid>
   );
 }
