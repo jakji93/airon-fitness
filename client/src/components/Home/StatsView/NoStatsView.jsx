1a77c6940862ecba37aa7ab2d49baeb248ae9507
chungs07 <chungs07@students.cs.ubc.ca>
2023-08-07 01:20:20 -0700

    Fix padding

--- client/src/components/Home/StatsView/NoStatsView.jsx
+++ client/src/components/Home/StatsView/NoStatsView.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 export default function NoStatsView() {
   return (
-    <Grid container alignItems="center" justifyContent="center" sx={{ p: 3, borderRadius: '10px', height: '15vw' }}>
+    <Grid container alignItems="center" justifyContent="center" sx={{ p: 2, height: '15vw' }}>
       <Typography variant="h5" component="span" color="text.secondary">
         Meal data not available. Please generate a plan first.
       </Typography>
